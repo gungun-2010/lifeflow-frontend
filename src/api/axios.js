1a186e8c9bee66e7aa3f56c792a5fde520c9b60b
Piyush Kumar <piyushkumar@Piyushs-MacBook-Air-2.local>
2026-06-28 10:09:38 +0530

    Use environment variable for API URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,31 +1,31 @@
-import axios from "axios";
-
-const API = axios.create({
-  baseURL: "http://localhost:5001/api",
-});
-
-export default API;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // import axios from "axios";
 
 // const API = axios.create({
-
-//   baseURL:
-//     "https://lifeflow-backend-5rm3.onrender.com/api"
-
+//   baseURL: "http://localhost:5001/api",
 // });
 
 // export default API;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import axios from "axios";
+
+const API = axios.create({
+
+  baseURL:
+    "https://lifeflow-backend-5rm3.onrender.com/api"
+
+});
+
+export default API;
